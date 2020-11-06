e16f6f0c4c37113c6ef9f9a6e4d672510750c61d
Henry Hirsch <henry@w3-net.de>
2020-11-06 21:02:35 +0100

    feat: very basic rc

--- cache/compiled/files/83ebd6b32ded0764d2dac651af3bfb3d.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/henry/system/projects/websites/w3-blog/user/config/security.yaml',
-    'modified' => 1604687528,
-    'data' => [
-        'salt' => 'qA7VZS32pJWXwv'
-    ]
-];
---
+++ user/pages/02.blog/blog-setup/item.md
@@ -0,0 +1,5 @@
+---
+title: New CMS Software
+---
+I have replaced the old software with a fresh [Grav](https://getgrav.org) CMS page.
+Grav is a simple to use and very performant flat file CMS written in PHP.
